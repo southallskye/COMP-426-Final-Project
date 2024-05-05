6f716fbb1dab5736b1572d6aba594a9bb8353aee
mako624 <mako624@email.unc.edu>
2024-05-05 14:37:30 -0400

    events_db

--- comp-426-final-project/setup_db.mjs
+++ comp-426-final-project/setup_db.mjs
@@ -7,9 +7,22 @@ db.run(`CREATE TABLE users (
 )`);
 
 db.run(`CREATE TABLE trips (
+    trip_id INTEGER,
+    user TEXT,
     start_date TEXT,
     end_date TEXT,
-    location TEXT
+    location TEXT,
+    FOREIGN KEY user REFERENCES users (username)
 )`);
+
+db.run(`CREATE TABLE events (
+    trip_id INTEGER,
+    event_id INTEGER,
+    date TEXT,
+    desc TEXT,
+    FOREIGN KEY trip_id REFERENCES trips (trip_id)
+)`)
+
+db.run()
 
 db.close()
