201902d593a811097b93275626b0907e94b9258d
Alex Matilainen <mako624@email.unc.edu>
2024-05-05 03:35:18 -0400

    db

--- comp-426-final-project/setup_db.mjs
+++ comp-426-final-project/setup_db.mjs
@@ -7,9 +7,18 @@ db.run(`CREATE TABLE users (
 )`);
 
 db.run(`CREATE TABLE trips (
+    trip_id INTEGER,
     start_date TEXT,
     end_date TEXT,
-    location TEXT
+    location TEXT,
+)`);
+
+db.run(`CREATE TABLE events (
+    trip_id INTEGER,
+    event_id INTEGER,
+    date TEXT,
+    desc TEXT,
+    FOREIGN KEY trip_id REFERENCES trips (trip_id)
 )`);
 
 db.close()
